03aaf5ba8b15cb70b7ca28e76e25ed196e0415f1
Juan Pablo Roca <jroca@shamanops.com>
2024-01-09 11:50:31 -0300

    initial

---
+++ server.js
@@ -0,0 +1,90 @@
+var express = require('express'),
+    async = require('async'),
+    { Pool } = require('pg'),
+    cookieParser = require('cookie-parser'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server);
+
+var port = process.env.PORT || 8080;
+var redis_url = process.env.REDIS_URL || 'redis-prod-master';
+var redis_user = process.env.REDIS_USER || 'redis';
+var redis_password = process.env.REDIS_PASSWORD || 'redis';
+
+io.on('connection', function (socket) {
+
+  socket.emit('message', { text : 'Welcome!' });
+
+  socket.on('subscribe', function (data) {
+    socket.join(data.channel);
+  });
+});
+
+// Variables para la conexión
+var db_user = process.env.DB_USER || 'postgresql';
+var db_password = process.env.DB_PASSWORD || 'postgresql';
+var db_url = process.env.DB_URL || 'postgresql-prod';
+var db_name = process.env.DB_NAME || 'postgresql';
+
+// Construir el connection string
+var connectionString = `postgres://${db_user}:${db_password}@${db_url}/${db_name}`;
+
+// Crear el objeto Pool con el connection string construido
+var pool = new Pool({
+  connectionString: connectionString
+});
+
+async.retry(
+  {times: 1000, interval: 1000},
+  function(callback) {
+    pool.connect(function(err, client, done) {
+      if (err) {
+        console.error("Waiting for db");
+      }
+      callback(err, client);
+    });
+  },
+  function(err, client) {
+    if (err) {
+      return console.error("Giving up");
+    }
+    console.log("Connected to db");
+    getVotes(client);
+  }
+);
+
+function getVotes(client) {
+  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
+    if (err) {
+      console.error("Error performing query: " + err);
+    } else {
+      var votes = collectVotesFromResult(result);
+      io.sockets.emit("scores", JSON.stringify(votes));
+    }
+
+    setTimeout(function() {getVotes(client) }, 1000);
+  });
+}
+
+function collectVotesFromResult(result) {
+  var votes = {a: 0, b: 0};
+
+  result.rows.forEach(function (row) {
+    votes[row.vote] = parseInt(row.count);
+  });
+
+  return votes;
+}
+
+app.use(cookieParser());
+app.use(express.urlencoded());
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname + '/views/index.html'));
+});
+
+server.listen(port, function () {
+  var port = server.address().port;
+  console.log('Application running on port ' + port);
+});
